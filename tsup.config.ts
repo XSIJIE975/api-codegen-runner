18067fe41d43acd72b1861827d274676dcea955f
XSIJIE975 <xsijie975@qq.com>
2025-12-03 17:15:28 +0800

    fix: update package.json and pnpm-lock.yaml for vite and typescript dependencies; modify tsup config to use ESM format

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,9 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   // index 用于导包，cli 用于命令行
   entry: ['src/index.ts', 'src/cli.ts'],
-  format: ['cjs', 'esm'],
+  format: 'esm',
   dts: true,
   clean: true,
-  // 注入垫片 (处理 ESM 环境下的 __dirname 等兼容性问题)
-  shims: true,
   // 不打包 node_modules (让它们作为依赖安装)
   skipNodeModulesBundle: true,
   // 关闭代码分割 (让 cli.js 和 index.js 独立，减少出错概率)
